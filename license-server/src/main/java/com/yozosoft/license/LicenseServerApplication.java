429fd8fc4fc7dca4cf895c384022dee7f1ce17e6
zhouf <513829633@qq.com>
2022-07-14 15:44:48 +0800

    1.fix: 解决body获取一次问题

--- license-server/src/main/java/com/yozosoft/license/LicenseServerApplication.java
+++ license-server/src/main/java/com/yozosoft/license/LicenseServerApplication.java
@@ -2,8 +2,10 @@ package com.yozosoft.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan("com.yozosoft.license")
 public class LicenseServerApplication {
 
     public static void main(String[] args) {
