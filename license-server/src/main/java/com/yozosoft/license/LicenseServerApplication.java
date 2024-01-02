633c53206d36f77f8e48aa27c20d4cfc36a360e4
13771509865 <513829633@qq.com>
2024-01-02 10:34:57 +0800

    Update LicenseServerApplication.java

--- license-server/src/main/java/com/yozosoft/license/LicenseServerApplication.java
+++ license-server/src/main/java/com/yozosoft/license/LicenseServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+@Author zhouf
+*/
 @SpringBootApplication
 @ServletComponentScan("com.yozosoft.license")
 public class LicenseServerApplication {
